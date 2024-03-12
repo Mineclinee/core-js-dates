9fb8b275a54962d782d410181c0487581811819c
Mineclinee <vasily-minecline@yandex.ru>
2024-03-12 15:21:01 +0300

    feat: add solution to some tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -113,8 +113,9 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  const date = new Date(year, month, 0);
+  return date.getDate();
 }
 
 /**
@@ -128,8 +129,11 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const start = new Date(dateStart);
+  const end = new Date(dateEnd);
+  const ms = end - start;
+  return Math.round(ms / (1000 * 60 * 60 * 24)) + 1;
 }
 
 /**
@@ -164,8 +168,15 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const data = new Date(date);
+  const options = {
+    timeZone: 'UTC',
+    hour12: 'numeric',
+  };
+  const times = data.toLocaleString('en-US', options);
+
+  return times;
 }
 
 /**
